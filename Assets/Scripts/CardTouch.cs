ccd241fe2b74e126ba7a652ca48ea25b4c97b95d
AjayBS <bsajay.22@gmail.com>
2017-02-11 14:23:52 -0700

    first commit

---
+++ Assets/Scripts/CardTouch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardTouch : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public static GameObject DraggedInstance;
+
+    Vector3 _startPosition;
+    Vector3 _offsetToMouse;
+    float _zDistanceToCamera;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnMouseEnter()
+    {
+     
+    }
+
+    void OnMouseExit()
+    {
+       
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DraggedInstance = gameObject;
+        _startPosition = transform.position;
+        _zDistanceToCamera = Mathf.Abs(_startPosition.z - Camera.main.transform.position.z);
+
+        _offsetToMouse = _startPosition - Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
+        );
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (Input.touchCount > 1)
+            return;
+
+        transform.position = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
+            ) + _offsetToMouse;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DraggedInstance = null;
+        _offsetToMouse = Vector3.zero;
+    }
+}
